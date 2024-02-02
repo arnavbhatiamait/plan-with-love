c50d32e81da6cc7bf5d9cba656370e80ca22e7fb
arnavbhatiamait <arnavbhatiamait@gmail.com>
2024-02-03 02:58:02 +0530

    publish

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,7 +13,7 @@ function Card({ id, image, info, price, name, removeTour }) {
     return (
 
         <div className="card">
-            <img src={image} className="image"></img>
+            <img src={image} className="image" alt=""></img>
             <div className="tour-info">
                 <div className="tour-detail">
                     <h4 className="tour-price">₹{price}</h4>
